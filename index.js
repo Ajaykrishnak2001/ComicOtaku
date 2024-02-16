7ee2ffbb3371344491249a40d1e40d226ca86fb1
Ajaykrishnak2001 <150816909+Ajaykrishnak2001@users.noreply.github.com>
2024-02-16 09:00:08 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app=express();
 // Ensure session middleware is set up before any routes are defined
 const session = require("express-session");
 app.use(session({
-    secret: "your-secret-key",
+    secret: "your-secret-key",;
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }
